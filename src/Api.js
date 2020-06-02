945ed860bc7f92836eb5aac631c6c4d778cc9faa
xabierMartinez42 <xabier.martinez@taiger.com>
2020-06-02 23:17:26 +0200

    Second commit

--- src/Api.js
+++ src/Api.js
@@ -3,22 +3,16 @@ import axios from 'axios'
 const SERVER_URL = 'https://euw1.api.riotgames.com'
 const API_KEY = 'RGAPI-cf23fb00-2a55-44b2-b8f7-8b4403102f1a'
 
-// let config = {
-//   headers: {
-//     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-//     'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-//     'Origin': 'https://developer.riotgames.com',
-//     'X-Riot-Token': API_KEY
-//   }
-// }
-
 const instance = axios.create({
   baseURL: SERVER_URL,
   timeout: 1000,
+  credentials: 'same-origin',
   headers: {
     'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
     'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
-    Origin: 'https://developer.riotgames.com',
+    'X-Riot-Token': API_KEY
+  },
+  params: {
     'X-Riot-Token': API_KEY
   }
 })
@@ -34,7 +28,7 @@ export default {
   },
 
   getSummonerInfo (sumName) {
-    return this.execute('GET', 'lol/summoner/v4/summoners/by-name/' + sumName, null, {
+    return this.execute('GET', 'lol/summoner/v4/summoners/by-name/' + sumName + '/', null, {
       transformResource: [function (data) {
         return data ? JSON.parse(data) : data
       }]
